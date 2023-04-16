4ef461099848a3d6534002b5735d781ebbe27e2d
Brenda2026 <118331486+Brenda2026@users.noreply.github.com>
2023-04-16 12:58:28 -0500

    Final Product

---
+++ src/React.js
@@ -0,0 +1,7 @@
+const sections = document.querySelectorAll('.sections');
+const sectBtns = document.querySelectorAll('.controls');
+const allSections= document.querySelector('.main-content');
+
+function PageTransition(){
+    //Button click active class
+}
